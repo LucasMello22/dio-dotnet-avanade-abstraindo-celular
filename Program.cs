26347e2834883f9780da6e4156c9e7bb91f816fb
Lucas Mello22 <lucas.s.mello@outlook.com>
2024-01-08 12:30:56 -0300

    finaly project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Abstraindo_celular.Models;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Smartphone NOKIA");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "NOKIA C20", imei: "1212121212", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "Iphone 14", imei: "2323232323", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
